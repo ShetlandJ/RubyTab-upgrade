aad409b48020bff76271f1ed31694f6eaaca5a76
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-09-19 15:37:36 +0100

    Updates the js file so some of the side links work

--- MyJs.js
+++ MyJs.js
@@ -8,7 +8,7 @@ var operators = [
   "<h3>Greater than and less than</h3><li>5 > 3 == true</li><li>10 < 7 == false</li><li>Handy tip: think of 'less than' as pointing left < </li><br>",
 
   "<h3>The main mathmetical operators you will use are:</h3><p>Addition: 3 + 5 == 8</p><p>Subtraction: 10 - 7 = 3</p><p>Mulitplication: 5 * 10 = 50</p><p>Division: 100 / 5 = 20</p><p>Exponent: 5**3 = 125 (multiply the first number by itself the number of times of the second number. In this example, 5*5*5.)</p><p>Modulus: 10%3 = 1 (divides the left hand operand by the right hand operand and returns the remainder, in this example, 10 / 3 returns 3 with 1 remainder)</p><br>"
-]
+];
 
 var loops_branching = [
   "<h3>if / else statements are presented like this:</h3><p>if condition</p><p>...</p><p>else</p><p>...</p><p>end</p><br>",
@@ -21,15 +21,15 @@ var loops_branching = [
   "<h3>until loops execute a block of code as long as a specific condition is met. Here's an until loop in action:</h3><p>i = 0</p><p>until i = 3 do</p><p>puts i</p><p>i += 1</p><p>end</p><p>We create a variable, which in this instance is 'i' but could be anything: 'variable', 'banana', 'foobar' etc, and we're telling the computer to run the code - to continue printing our variable - until it equals five. We then use our code to add a 1 to our variable incrementally. We use += to show incremental addition, which is coding shorthand for i = i + 1. We then show the result.</p><br>",
 
   "<h3>If/else statement isn't useful if you have a large numbers of conditions. That's where the switch statement comes in handy:</h3><p>print: \"Enter your grade: \"</p><p>age = gets.chomp # this asks for a user input and stores it as a variable</p><p>case grade</p><p>when \"A\"</p><p>puts \"Well done!\"</p><p>when \"B\"</p><p>puts \"Pretty good!\"</p><p>when \"C\"</p><p>puts \"Room for improvement!\"</p><p>when \"D\"</p><p>puts \"That's a fail!\"</p><p>else</p><p>puts \"That's not one of our grades!\"</p><br>"
-]
+];
 
 var print_to_console = [
   "<h3>Print text using puts</h3><p>puts \"Hello World\"<p>puts will place each item on a new line</p><p>puts [[1, 2], [3, 4]] will return as: <p>1</p><p>2</p><p>3</p><p>4</p><p>You can also use print, which will return each item on the same line:</p><p>print [[1, 2], [3, 4]] will return as:</p><p>[[1, 2], [3, 4]</p><br>"
-]
+];
 
 var commenting = [
   "<h3>You can use hashtags to add comments to explain your code:</h3><p># adds two numbers together</p><p>10 + 12</p><p># asks users for their input and stores it in a variable called 'age'</p><p>age = gets.chomp<p>If you want to comment out a large portion of code, place =begin in a line above it and =end below it.<br>"
-]
+];
 
 var variables = [
   "<h3>Use variables to store information that you can use later. You should aim to name your variable approriate to the content it will contain:</h3><p>my_greeting = \"Hello, user!\"</p><p>And here is how we would then call that greeting:</p><p>puts my_greeting</p><br>",
@@ -37,41 +37,40 @@ var variables = [
   "<h3>Here's another way to use variables:</h3><p>my_name = \"John\"</p><p>puts \"Hello there, my name is #{my_name}\"</p><p>Alternatively, you can call your variable using %s:</p><p>puts \"Hello, my name is %s\" % my_name</p><p>puts \"Hello, my name is %s and I'm %s years old % [\"James\", 25]\"<br>",
 
   "<h3>If you want to quote inside a string, you can't just say \"I said \"hello\" to John\" because Ruby will think that the \" around \"hello\" is the end of the string. You can use backslashes to help you here:</h3><p>\"I am 6'2\\\" tall.\"</p><p>\'I am 6\\\'2\" tall.\'</p><br>"
-]
+];
 
 var get_string = [
   "<h3>If you want to prompt the user for input, you would use:</h3><p>gets.chomp</p><p>But what if we want to use the information that is submitted? We can store it in a variable:</p><p>puts \"What is your name?\"</p><p>input = gets.chomp</p><p># the user will be prompted to submit information</p><p>puts \"Hello, #{input}, thanks for giving us your name!\"</p><br>"
-]
+];
 
 var arrays = [
   "<h3>An array is a way of storing objects in Ruby. It's coded like so:</h3><p>my_array = [\"James\", 4, \"It can contain really long strings, or numbers, or, well, just about anything\"]</p><p>You're actually seeing an array in action with this extension. All the text that you see in these tips is stored in an array, and javascript is telling the array to randomly display a tip each time you reload, open a new tab, or click 'next tip'!</p><p>You can see the source code for that array by <a href=\"https://github.com/ShetlandJ/learnruby/blob/master/MyJs.js\" target=\"_blank\">clicking here!</a></p><br>",
 
   "<h3>Multi-dimensional arrays are arrays that contain other arrays!</h3><p>my_multi_array = [[1, 2, 3], [4, 5], [\"John\", 7, 163], [[40, 50], [66, 77]]]</p><br>"
-]
+];
 
-var hashes = []
+var hashes = [];
 
-var methods = []
+var methods = [];
 
-var ranges = []
+var ranges = [];
 
-var writing_to_files = []
+var writing_to_files = [];
 
-var irb = []
+var irb = [];
 
-var blocks = []
+var blocks = [];
 
-var procs = []
+var procs = [];
 
-var myArray = [operators, arrays]
+//var myArray = [operators, loops_branching, print_to_console, commenting, variables, get_string, arrays]
+var myArray = [operators, loops_branching, arrays]
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
     var x = myArray[Math.floor(Math.random() * myArray.length)][Math.floor(Math.random() * myArray.length)];
-      document.getElementById("container").innerHTML = x;
+    document.getElementById("container").innerHTML = x;
   });
-
-
 
 function myFunction() {
   var x = myArray[Math.floor(Math.random() * myArray.length)][Math.floor(Math.random() * myArray.length)];
@@ -79,11 +78,50 @@ function myFunction() {
 };
 
 // Sidebar links
-
+//arrays
 document.getElementById("array").addEventListener("click", arrayFunction);
-
 
 function arrayFunction() {
   var x = arrays[Math.floor(Math.random() * arrays.length)];
+  document.getElementById("container").innerHTML = x;
+};
+
+//commenting
+document.getElementById("commenting").addEventListener("click", commentFunction);
+
+function commentFunction() {
+  var x = commenting[Math.floor(Math.random() * commenting.length)];
+  document.getElementById("container").innerHTML = x;
+};
+
+//loops
+document.getElementById("loops").addEventListener("click", loopFunction);
+
+function loopFunction() {
+  var x = loops_branching[Math.floor(Math.random() * loops_branching.length)];
+  document.getElementById("container").innerHTML = x;
+};
+
+//operators
+document.getElementById("operators").addEventListener("click", opFunction);
+
+function opFunction() {
+  var x = operators[Math.floor(Math.random() * operators.length)];
+  document.getElementById("container").innerHTML = x;
+};
+
+//print
+document.getElementById("print").addEventListener("click", printFunction);
+
+function printFunction() {
+  var x = print_to_console[Math.floor(Math.random() * print_to_console.length)];
+  document.getElementById("container").innerHTML = x;
+};
+
+//print
+document.getElementById("variables").addEventListener("click", varFunction);
+
+function varFunction() {
+  var x = variables[Math.floor(Math.random() * variables.length)];
   document.getElementById("container").innerHTML = x;
 };
